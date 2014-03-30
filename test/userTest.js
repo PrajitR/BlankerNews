0350cc3a933615030b62ff36f205863dfd1951d1
Prajit Ramachandran <ramprajit@gmail.com>
2014-03-30 10:08:48 -0700

    Tests

---
+++ test/userTest.js
@@ -0,0 +1,43 @@
+var should = require('should');
+var mongoose = require('mongoose');
+var Account = require('../models/account');
+var db;
+
+describe('Account', function () {
+
+  before(function (done) {
+    db = mongoose.connect('mongodb://localhost/test');
+    done();
+  });
+
+  after(function (done) {
+    mongoose.connection.close();
+    done();
+  });
+
+  beforeEach(function (done) {
+    var account = new Account({
+      username: '12345',
+      password: 'testy'
+    });
+
+    account.save(function (error) {
+      if (error) console.error('Error: ' + error.message);
+      done();
+    });
+  });
+
+  it('find a user by username', function (done) {
+    Account.findOne({ username: '12345' }, function (err, account) {
+      account.username.should.eql('12345');
+      done();
+    });
+  });
+
+  afterEach(function (done) {
+    Account.remove({}, function () {
+      done();
+    });
+  });
+
+});
